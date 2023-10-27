85e44af9d5294f104900246a755d35217e8b073b
Rodri Saiz <rodrisaiz@icloud.com>
2023-10-27 18:25:57 +0200

    initial creation of Models and Controllers for Notes, Authors and Categories

---
+++ database/migrations/2023_10_27_160739_create_notes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->date('date');
+            $table->text('content_A');
+            $table->text('content_B');
+            $table->binary('img_1');
+            $table->binary('img_2');
+            $table->binary('img_3');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+    }
+};
